8ee58a955504f53b0a7b515c729a06bb38d1734d
Amitesh <amiteshr90@gmail.com>
2019-08-28 01:30:19 +0530

    Feature addition: fetch geolocation

--- src/components/App.js
+++ src/components/App.js
@@ -6,41 +6,61 @@ class App extends Component {
 	    this.state = {
 	      error: null,
 	      isLoaded: false,
-	      dummy: []
-	      
+	      dummy: [],
+	      geolocation: null
 	    };
 	  }
 
-	  componentDidMount() {
-	    this.fetchDummy();
-	    // dispatch({ type: AGENT_LIST_FETCHED })
-	  }
+	componentDidMount() {
+		this.getGeoLocation();
+		this.fetchDummy();
+		// dispatch({ type: AGENT_LIST_FETCHED })
+	}
+
+	getGeoLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				this.setState({
+					geolocation: {
+						lat: position.coords.latitude,
+						long: position.coords.longitude
+					}
+				})
+			});
+		} else { 
+			this.state.geolocation = null;
+		}
+	}
 
 	fetchDummy() {
 	    fetch("https://api.myjson.com/bins/wrme3")
-	      .then(res => res.json())
-	      .then(
-	        (result) => {
-	          this.setState({
-	            isLoaded: true,
-	            dummy: result.a
-
-	          });
-	          console.log(this.state.dummy);
-	        },
-	        (error) => {
-	          this.setState({
-	            isLoaded: true,
-	            error
-	          });
-	        }
-	      )
+			.then(res => res.json())
+			.then(
+				(result) => {
+					this.setState({
+						isLoaded: true,
+						dummy: result.a
+					});
+				},
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			)
 	}
 
 	render () {
 		return (
 				<div>
-					<h1>My App in React</h1>
+					<h1>Geolocation: </h1>
+					{(this.state.geolocation && (
+						<div>
+							{this.state.geolocation.lat + '  ' + this.state.geolocation.long}
+						</div>
+						)
+					)}
 					{(this.state.dummy && (this.state.dummy.map(function(elem){
 						return (
 							<div key={elem.c}>{elem.b}</div>
